a288bb9a69d2cdb05c609c7b0c9a389efc66d30b
mareike.taeubner <mareike.taeubner@procilon.de>
2024-07-27 11:02:38 +0200

    added typing

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+  /**
+   * Simple sync conversion from tif to png.
+   *
+   * @param {Uint8Array} tif the binary tif image data
+   * @returns {Uint8Array} the png image data
+   */
+export declare function tif2png(tif:Uint8Array): Uint8Array
